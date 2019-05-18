aad30913c6556b2d56f164f5e2cf21c6d5f31003
Evan Tahler <evan.tahler@voom.flights>
2019-05-17 22:53:43 -0700

    start code server at boot

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,6 @@ echo "linking 'workstation-setup' command"
 ln -s ~/workspace/workstation/workstation-setup.sh /usr/local/bin/workstation-setup
 
 # Run the fnial docker entrypoint command
-echo "*** starting up SSH on port $WORKSTATION_SSH_PORT ***"
-/usr/sbin/sshd -D
+echo "*** starting up SSH on port $WORKSTATION_SSH_PORT and Code Server***"
+/usr/sbin/sshd -D 
+cd ~/workspace && code-server .
